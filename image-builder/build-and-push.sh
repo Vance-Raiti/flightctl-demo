466d73632741ee2de3f6581b6c866f4e5d1b52ac
Vance Raiti <vraiti77@gmail.com>
2024-06-08 14:02:06 -0400

    basic pipeline

--- image-builder/build-and-push.sh
+++ image-builder/build-and-push.sh
@@ -2,10 +2,11 @@
 
 set -x
 
-su podman
+cd ~
 
-cd /git-repo/build-context
+git clone $REPO repo
+cd repo/build-context
 
 podman login -u $PODMAN_USER -p $PODMAN_PASSWORD $PODMAN_REGISTRY
 podman build -t $IMAGE_TAG .
-podman push $PODMAN_REGISTRY/$PODMAN_USER/$IMAGE_TAG
+podman push $IMAGE_TAG $PODMAN_REGISTRY/$PODMAN_USER/$IMAGE_TAG
